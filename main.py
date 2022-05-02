1d18aca31242218b32520e7beec6cd93a914f985
Augusto Toledo <atoledo@pcorp.cl>
2022-05-02 13:59:32 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from starlette.requests import Request
+from starlette.responses import Response
+from endpoint import endpoint
+import httpx
+
+DESTINATION_ENDPOINT:str = endpoint
+
+async def app(scope, receive, send):
+    request = Request(scope, receive)
+    url = DESTINATION_ENDPOINT + request.url.path
+    content = httpx.get(url)
+    response = Response(content.text, media_type="application/json")
+    await response(scope, receive, send)
