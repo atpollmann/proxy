eb6bd15480e932c3a1652f56d3c9c901e5e20e3d
Augusto Toledo <atoledo@pcorp.cl>
2022-05-03 17:06:49 -0400

    WIP

---
+++ request-worker/request-worker/config/__init__.py
@@ -0,0 +1,4 @@
+import os
+
+HOST_BROKER = os.getenv("HOST_BROKER")
+EXCHANGE_NAME = os.getenv("EXCHANGE_NAME")
--- request-worker/request-worker/main.py
+++ request-worker/request-worker/main.py
@@ -1,12 +1,28 @@
+import httpx
+import pika
+import json
+
 from starlette.requests import Request
 from starlette.responses import Response
 from endpoint import endpoint
-import httpx
+from config import EXCHANGE_NAME, HOST_BROKER
 
-DESTINATION_ENDPOINT:str = endpoint
+DESTINATION_ENDPOINT: str = endpoint
+
 
 async def app(scope, receive, send):
     request = Request(scope, receive)
+
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=HOST_BROKER))
+    channel = connection.channel()
+
+    channel.exchange_declare(exchange=EXCHANGE_NAME, exchange_type='fanout')
+    message = {'ip': request.client.host, 'path': request.url.path}
+
+    channel.basic_publish(exchange=EXCHANGE_NAME, routing_key='', body=json.dumps(message))
+    print("REQUEST WORKER: message send to exchange")
+    connection.close()
+
     url = DESTINATION_ENDPOINT + request.url.path
     content = httpx.get(url)
     response = Response(content.text, media_type="application/json")
