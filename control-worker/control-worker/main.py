1a380a2173be0ac8259dfcbef68fd0b4f181f415
Augusto Toledo <atoledo@pcorp.cl>
2022-05-03 20:56:34 -0400

    WIP

--- control-worker/control-worker/main.py
+++ control-worker/control-worker/main.py
@@ -2,6 +2,8 @@ import redis
 import pika
 import sys
 import os
+import json
+import time
 
 from pymongo import MongoClient
 from config import HOST_CONTROL_DB, HOST_CACHE_MASTER, HOST_BROKER, EXCHANGE_NAME
@@ -12,7 +14,7 @@ mongoClient = MongoClient(HOST_CONTROL_DB)
 
 def main():
     connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host="broker"))
+        pika.ConnectionParameters(host=HOST_BROKER))
     channel = connection.channel()
     channel.exchange_declare(exchange=EXCHANGE_NAME, exchange_type='fanout')
 
@@ -20,14 +22,25 @@ def main():
     queue_name = result.method.queue
 
     channel.queue_bind(exchange=EXCHANGE_NAME, queue=queue_name)
-    print('CONTROL WORKER: waiting for requests. To exit press CTRL+C')
-
-    def callback(ch, method, properties, body):
-        sys.exit(0)
 
     channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
 
     channel.start_consuming()
+
+
+def callback(ch, method, properties, body):
+    request = json.loads(body)
+    proxy_db = mongoClient.proxy_db
+    access_list = proxy_db.access_list
+
+    doc = access_list.find_one({"type": "counter", "ip": request["ip"], "path": request["path"]})
+    timestamp = int(time.time())
+    if doc is None:
+        access_list.insert_one(
+            {"type": "counter", "ip": request["ip"], "path": request["path"], "count": 1, "first": timestamp,
+             "last": timestamp})
+    else:
+        access_list.update_one({"_id": doc.get('_id')}, {'$inc': {'count': 1}, '$set': {'last': timestamp}})
 
 
 if __name__ == '__main__':
